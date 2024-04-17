ab107d9d5d59408233674ace5d9122a4bbaaf1e3
Sarah Oliveira <soliveirarm@outlook.com>
2024-04-17 00:43:37 -0300

    feat(wishlist): add book input

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  newBook: string = '';
+  books: any = [];
+  localSave: any = localStorage.getItem('books');
 
   constructor() {}
 
+  addBook() {
+    this.books.push(this.newBook);
+    this.newBook = '';
+  }
 }
